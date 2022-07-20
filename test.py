f4e6802057a677a5c75fba3840ff694e958ed683
paulohgon <paulohgon01@gmail.com>
2022-07-20 09:08:27 -0300

    muda os textos...

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import pytest
 class TestClass:    
 
     def test(self):
-        question = perguntas("qual seu nome caralho",2)
-        respostaa = resposta({"qual seu nome caralho":True},"paulogaraio")
+        question = perguntas("qual seu nome ",2)
+        respostaa = resposta({"qual seu nome ":True},"paulogaraio")
 
         #question.addresposta("bom dia")
 
         
-        assert respostaa.getrespostacerta("qual seu nome caralho") == "paulogaraio"
+        assert respostaa.getrespostacerta("qual seu nome") == "paulo"
 
 # question = perguntas("qual seu nome caralho",2)
 # respostaa = resposta({"qual seu nome caralho":True},"paulogaraio")
