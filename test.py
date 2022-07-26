b4183c1ef17ef97570b45a65456f8dd8515c6d50
Admin <bertemesbrian@gmail.com>
2022-07-25 21:58:22 -0300

    .

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ class TestClass:
     def test2(self):#testa os pontos 
         respostaa = resposta({"qual a capital da amazonia":True},"ratanaba")
         question = perguntas("qual a capital da amazonia",2)
-        aluno = estudante("paulo","paulo@paulo","masculo","30")
+        aluno = estudante("paulo","paulo@paulo","masculino","30")
         if respostaa.getrespostacerta("qual a capital da amazonia")=='ratanaba':
             maispontos = question.getpontos()
             aluno.addpontos(maispontos)
@@ -30,24 +30,24 @@ class TestClass:
         assert correta == "ratanaba"
 
     def test4(self):#cria sala pelo professor
-        professor1 = professor("pai do brian","paidobrian@paizao.com","homem masculino macho",2,"nada","udesc")
+        professor1 = professor("pedro","pedro@pedro.com","masculino",54,"EE","udesc")
         salacriada = professor1.criasala("ECC0001")
         assert salacriada.getnomesala()=="ECC0001"
 
     def test5(self):#remove um aluno da sala
-        professor1 = professor("pai do brian","paidobrian@paizao.com","homem masculino macho",2,"nada","udesc")
+        professor1 = professor("pedro","pedro@pedro.com","masculino",54,"EE","udesc")
         salacriada = professor1.criasala("ECC0001")
-        aluno1 = estudante("brian","filhodopaidobria@filhao","loiro",24)
-        aluno2 = estudante("bruno","amigodofilhodopaidobraia@amigao","3vezesnasemana",13)
+        aluno1 = estudante("brian","brian@brian","masculino",20)
+        aluno2 = estudante("bruno","bruno@bruno","masculino",21)
         professor1.addalunosala("ECC0001",[aluno1,aluno2])
         professor1.removealunosala("ECC0001",[aluno1])
         assert len(salacriada.getalunos()) == 1
 
     def test6(self):#testa se o aluno esta na sala adicionando pelo professor
-        professor1 = professor("pai do brian","paidobrian@paizao.com","homem masculino macho",2,"nada","udesc")
+        professor1 = professor("pedro","pedro@pedro.com","masculino",54,"EE","udesc")
         salacriada = professor1.criasala("ECC0001")
-        aluno1 = estudante("brian","filhodopaidobria@filhao","loiro",24)
-        aluno2 = estudante("bruno","amigodofilhodopaidobraia@amigao","3vezesnasemana",13)
+        aluno1 = estudante("brian","brian@brian","masculino",20)
+        aluno2 = estudante("bruno","bruno@bruno","masculino",21)
         professor1.addalunosala("ECC0001",[aluno1,aluno2])
 
         assert len(salacriada.getalunos()) == 2
